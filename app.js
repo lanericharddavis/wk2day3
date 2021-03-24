9f14765008395bd36614efec35e6b8101ca9e5b5
Lane <lanericharddavis@gmail.com>
2021-03-24 09:42:38 -0600

    different methods

--- app.js
+++ app.js
@@ -7,8 +7,29 @@ function isThisEvenOfOdd(num) {
     console.log(`${num} ${"is an odd number"} `)
   }
 }
-
 isThisEvenOfOdd(5)
+
+
+//Class Take on it
+function evenOrOdd(num) {
+  return i % 2 ? "odd" : "even"
+}
+//return i % 2 ? "if true" : "if false"
+console.log(evenOrOdd(2)) // 'even'
+console.log(evenOrOdd(3)) // 'odd'
+console.log(evenOrOdd(0)) // 'even'
+console.log(evenOrOdd(-2)) // 'even'
+console.log(evenOrOdd(-1)) // 'odd'
+
+//most likly writen like this cause it's easy to understand for other developers
+
+function evenOrOdd(num) {
+  if (num % 2 == 0) {
+    return "even"
+  }
+  return "odd"
+}
+
 
 
 //Count up to a certain number indicating if number is
