30751333a5e34be5d85ee5ccef66d5dc8699769b
Lane <lanericharddavis@gmail.com>
2021-03-24 09:49:20 -0600

    linked css correctly

--- app.js
+++ app.js
@@ -1,48 +1,49 @@
-
-//Tell if Number is Even or Odd Function
-function isThisEvenOfOdd(num) {
-  if (num % 2 === 0) {
-    console.log(`${num} ${"is an even number"} `)
-  } else {
-    console.log(`${num} ${"is an odd number"} `)
-  }
-}
-isThisEvenOfOdd(5)
-
-
-//Class Take on it
-function evenOrOdd(num) {
-  return i % 2 ? "odd" : "even"
-}
-//return i % 2 ? "if true" : "if false"
-console.log(evenOrOdd(2)) // 'even'
-console.log(evenOrOdd(3)) // 'odd'
-console.log(evenOrOdd(0)) // 'even'
-console.log(evenOrOdd(-2)) // 'even'
-console.log(evenOrOdd(-1)) // 'odd'
-
-//most likly writen like this cause it's easy to understand for other developers
-
-function evenOrOdd(num) {
-  if (num % 2 == 0) {
-    return "even"
-  }
-  return "odd"
-}
+//MORNING CHALLENGE
+// //Tell if Number is Even or Odd Function
+// function isThisEvenOfOdd(num) {
+//   if (num % 2 === 0) {
+//     console.log(`${num} ${"is an even number"} `)
+//   } else {
+//     console.log(`${num} ${"is an odd number"} `)
+//   }
+// }
+// isThisEvenOfOdd(5)
 
 
+// //Class Take on it
+// function evenOrOdd(num) {
+//   return i % 2 ? "odd" : "even"
+// }
+// //return i % 2 ? "if true" : "if false"
+// console.log(evenOrOdd(2)) // 'even'
+// console.log(evenOrOdd(3)) // 'odd'
+// console.log(evenOrOdd(0)) // 'even'
+// console.log(evenOrOdd(-2)) // 'even'
+// console.log(evenOrOdd(-1)) // 'odd'
 
-//Count up to a certain number indicating if number is
-console.log("Start the Count!")
+// //most likly writen like this cause it's easy to understand for other developers
 
-function oddOrEven(upToNum) {
-  for (let i = 0; i <= upToNum; i++) {
-    if (i % 2 === 0) {
-      console.log(`${i} ${"even"} `)
-    } else {
-      console.log(`${i} ${"odd"} `)
-    }
-  }
-}
+// function evenOrOdd(num) {
+//   if (num % 2 == 0) {
+//     return "even"
+//   }
+//   return "odd"
+// }
 
-oddOrEven(20)
+
+
+// //Count up to a certain number indicating if number is
+// console.log("Start the Count!")
+
+// function oddOrEven(upToNum) {
+//   for (let i = 0; i <= upToNum; i++) {
+//     if (i % 2 === 0) {
+//       console.log(`${i} ${"even"} `)
+//     } else {
+//       console.log(`${i} ${"odd"} `)
+//     }
+//   }
+// }
+
+// oddOrEven(20)
+
